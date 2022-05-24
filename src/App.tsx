62f1cc7a7cf70fbfb9c4c3127555d2ee0d3c3fe2
StepashkaSuicide <churbakov.evgen@mail.ru>
2022-05-24 20:41:23 +0300

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
 import React from 'react';
-import Cars from "./Components/Cars";
+import Cars from "./Cars";
+import {Button} from "./Components/Buttons/Button";
 
 
-
+const topCars = [
+    {id: 1, manufacturer: 'BMW', model: 'm5cs'},
+    {id: 2, manufacturer: 'Mercedes', model: 'e63s'},
+    {id: 3, manufacturer: 'Audi', model: 'rs6'}
+]
 
 function App() {
 
-    const topCars = [
-        {id: 1, manufacturer:'BMW', model:'m5cs'},
-        {id: 2, manufacturer:'Mercedes', model:'e63s'},
-        {id: 3, manufacturer:'Audi', model:'rs6'}
-    ]
-  return (
-    <>
-<Cars topCars={topCars}/>
+    const buttonFoo1 = (subscriber: string) => {
+        console.log(subscriber)
+    }
+    const buttonFoo2 = (subscriber: string) => {
+        console.log(subscriber)
+    }
+const buttonFoo3 = () => {
+        console.log(`I'm stupid button` )
+    }
 
+    return (
+        <>
+            <Cars topCars={topCars}/>
+            <Button name={'PROJECT JIJA - 1'} callBack={() => buttonFoo1('Hi')}/>
+            <Button name={'PROJECT JIJA - 2'} callBack={() => buttonFoo2('bye')}/>
+            <Button name={'PROJECT JIJA - 3'} callBack={buttonFoo3}/>
 
-    </>
-  );
+        </>
+    );
 }
 
 export default App;

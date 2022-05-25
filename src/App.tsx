37b4da96d9d7e09a2c8929f837465a467e2780e8
StepashkaSuicide <churbakov.evgen@mail.ru>
2022-05-25 11:32:59 +0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Cars from "./Cars";
 import {Button} from "./Components/Buttons/Button";
 
@@ -11,25 +11,49 @@ const topCars = [
 
 function App() {
 
-    const buttonFoo1 = (subscriber: string) => {
-        console.log(subscriber)
+//     const buttonFoo1 = (subscriber: string) => {
+//         console.log(subscriber)
+//     }
+//     const buttonFoo2 = (subscriber: string) => {
+//         console.log(subscriber)
+//     }
+// const buttonFoo3 = () => {
+//         console.log(`I'm stupid button` )
+//     }
+//
+//     return (
+//         <>
+//             <Cars topCars={topCars}/>
+//             <Button name={'PROJECT JIJA - 1'} callBack={() => buttonFoo1('Hi')}/>
+//             <Button name={'PROJECT JIJA - 2'} callBack={() => buttonFoo2('bye')}/>
+//             <Button name={'PROJECT JIJA - 3'} callBack={buttonFoo3}/>
+//
+//         </>
+//     );
+
+    let [a, setA] = useState(1)
+
+    const onClickHandler =() => {
+        setA(++a)
     }
-    const buttonFoo2 = (subscriber: string) => {
-        console.log(subscriber)
-    }
-const buttonFoo3 = () => {
-        console.log(`I'm stupid button` )
+    const onClickHandler1 =() => {
+        setA(0)
     }
 
-    return (
-        <>
-            <Cars topCars={topCars}/>
-            <Button name={'PROJECT JIJA - 1'} callBack={() => buttonFoo1('Hi')}/>
-            <Button name={'PROJECT JIJA - 2'} callBack={() => buttonFoo2('bye')}/>
-            <Button name={'PROJECT JIJA - 3'} callBack={buttonFoo3}/>
 
-        </>
-    );
+    return(
+        <div>
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={onClickHandler1}>0</button>
+        </div>
+    )
+
+
 }
+
+
+
+
 
 export default App;

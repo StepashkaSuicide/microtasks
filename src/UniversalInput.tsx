ef8365218ea54a4a53f8255d06918b3eda12aa6f
StepashkaSuicide <churbakov.evgen@mail.ru>
2022-06-30 12:51:40 +0300

    practice 6 lesson

--- src/UniversalInput.tsx
+++ src/UniversalInput.tsx
@@ -1,31 +1,19 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 
 
-type InputPropsType = {
-    callBack: (title: string)=>void
+type TopInputType = {
+    addItem: (title: string )=> void
+
 }
 
 
-
-export const Input = (props: InputPropsType) => {
-
-    const [title, setTitle] = useState("")
-    const [error, setError] = useState<string | null>(null)
-
-    const addTask = () => {
-        let newTitle = title.trim();
-        if (newTitle !== "") {
-            props.callBack(newTitle);
-            setTitle("");
-        } else {
-            setError("Title is required");
-        }
-    }
+export const TopInput = (props: TopInputType) => {
+    let [title, setTitle] = useState('')
+    let [error, setError] = useState<string | null>(null)
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setTitle(e.currentTarget.value)
     }
-
     const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
         setError(null);
         if (e.charCode === 13) {
@@ -33,17 +21,26 @@ export const Input = (props: InputPropsType) => {
         }
     }
 
-
+    const addTask = () => {
+        let newTitle = title.trim();
+        if (newTitle !== '') {
+            props.addItem(newTitle);
+            setTitle('');
+        } else {
+            setError('Title is required');
+        }
+    }
 
     return (
         <div>
             <input value={title}
                    onChange={onChangeHandler}
                    onKeyPress={onKeyPressHandler}
-                   className={error ? "error" : ""}
+                   className={error ? 'error' : ''}
             />
             <button onClick={addTask}>+</button>
             {error && <div className="error-message">{error}</div>}
         </div>
-    );
-};
+    )
+
+}

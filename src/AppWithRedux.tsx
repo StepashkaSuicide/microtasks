5341969c3cd5785af58624babaa200906c05d29f
StepashkaSuicide <churbakov.evgen@mail.ru>
2022-07-29 16:11:39 +0300

    REDUXXXXXXXXXX refactoring

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
-import {TaskType, Todolist} from './Todolist';
+import {Todolis1t} from './Todolis1t';
 import {Input} from './InputXX';
-import {addTodolistAC, changeTodolistFilterAC, editTodolistAC, removeTodolistAC} from './state/todolists-reducer';
-import {addTaskAC, changeTaskStatusAC, editTaskAC, removeTaskAC} from './state/tasksReducer';
+import {addTodolistAC} from './state/todolists-reducer';
 import {useDispatch, useSelector} from 'react-redux';
 import {AppRootStateType} from './state/store';
 
@@ -15,44 +14,14 @@ export type TodolistType = {
     filter: FilterValuesType
 }
 
-export type TasksStateType = {
-    [key: string]: Array<TaskType>
-}
 
 export function AppWithRedux() {
     const todolists = useSelector<AppRootStateType, Array<TodolistType>>(state=>state.todolists)
-    const tasks = useSelector<AppRootStateType, TasksStateType>(state=>state.tasks)
     const dispatch = useDispatch()
 
-    function removeTask(taskID: string, todolistId: string) {
-        dispatch(removeTaskAC(taskID, todolistId))
-    }
-
-    function addTask(title: string, todolistId: string) {
-        dispatch(addTaskAC(title, todolistId))
-    }
-
-    function changeStatus(taskID: string, isDone: boolean, todolistId: string) {
-        dispatch(changeTaskStatusAC(taskID, isDone, todolistId))
-    }
-
-    function changeFilter(value: FilterValuesType, todolistId: string) {
-        dispatch(changeTodolistFilterAC(value, todolistId))
-    }
-
-    function removeTodolist(id: string) {
-        dispatch(removeTodolistAC(id))
-    }
 
     const addTodolist = (title: string)=> {
-        const action = addTodolistAC(title)
-        dispatch(action)
-    }
-    const editTodolist = (todolistId: string, newTitle: string)=> {
-        dispatch(editTodolistAC(todolistId, newTitle ))
-    }
-    const editTask=(todolistId: string, taskID: string, newTitle: string)=> {
-        dispatch(editTaskAC(todolistId, taskID, newTitle))
+        dispatch(addTodolistAC(title))
     }
 
 
@@ -61,29 +30,20 @@ export function AppWithRedux() {
             <Input callBack={addTodolist} />
             {
                 todolists.map(tl => {
-                    let allTodolistTasks = tasks[tl.id];
-                    let tasksForTodolist = allTodolistTasks;
+                    // let allTodolistTasks = tasks[tl.id];
+                    // let tasksForTodolist = allTodolistTasks;
+                    //
+                    // if (tl.filter === "active") {
+                    //     tasksForTodolist = allTodolistTasks.filter(t => t.isDone === false);
+                    // }
+                    // if (tl.filter === "completed") {
+                    //     tasksForTodolist = allTodolistTasks.filter(t => t.isDone === true);
+                    // }
 
-                    if (tl.filter === "active") {
-                        tasksForTodolist = allTodolistTasks.filter(t => t.isDone === false);
-                    }
-                    if (tl.filter === "completed") {
-                        tasksForTodolist = allTodolistTasks.filter(t => t.isDone === true);
-                    }
-
-                    return <Todolist
+                    return <Todolis1t
                         key={tl.id}
-                        id={tl.id}
-                        title={tl.title}
-                        tasks={tasksForTodolist}
-                        removeTask={removeTask}
-                        changeFilter={changeFilter}
-                        addTask={addTask}
-                        changeTaskStatus={changeStatus}
-                        filter={tl.filter}
-                        removeTodolist={removeTodolist}
-                        editTodolist={editTodolist}
-                        editTask={editTask}
+                        todolist={tl}
+
                     />
                 })
             }
